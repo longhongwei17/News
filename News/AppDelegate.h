9488b1db2ca473e6ee2fe8267ca235b38b2eaf2d
龙伟 <longhongwei17@163.com>
2016-03-23 21:29:04 +0800

    Initial Commit

---
+++ News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by longhongwei on 16/3/23.
+//  Copyright © 2016年 longhongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
